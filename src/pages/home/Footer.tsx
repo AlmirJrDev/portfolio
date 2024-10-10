7b3ebd944394160a1b55a0c9e69be30e3f33baf1
Almir Júnior Araújo Gomes <99154364+AlmirJrDev@users.noreply.github.com>
2024-10-10 11:52:24 -0300

    Footer homepage media query

--- src/pages/home/Footer.tsx
+++ src/pages/home/Footer.tsx
@@ -8,15 +8,15 @@ export function Footer() {
       
       className='border-t-2 border-white'>
         <div className='pl-60 pr-60 mt-20 mb-20 max-lg:p-5'>
-          <div className='flex justify-between items-center'>
+          <div className='flex justify-between items-center max-lg:flex-col '>
             <div>
-            <div className='flex gap-8'>   
+            <div className='flex gap-8 max-lg:flex-col max-lg:flex max-lg:justify-center max-lg:items-center'>   
               <h2 className='text-white'>AlmirJrDev</h2> <a className='text-gray-500' href="mailto:almirjrag@gmail.com">almirjrag@gmail.com</a>
             </div>
-            <span className='text-gray-500'>Developer Front-end</span>
+            <span className='text-gray-500 max-lg:mb-2'>Developer Front-end</span>
           </div>
 
-          <div className='flex gap-4'>
+          <div className='flex gap-4 max-lg:mt-4'>
             <a href="#"><Github className='size-5 text-white'/></a>
             <a href="#"><LinkedinIcon className='size-5 text-white'/></a>
           </div>
