c8e59a585725de12148366f48e94dc8b24bb3d74
Almir Júnior Araújo Gomes <99154364+AlmirJrDev@users.noreply.github.com>
2025-04-04 17:06:46 -0300

    fix: imgs url changes

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -24,7 +24,7 @@ Se você está procurando alguém com paixão, dedicação e sede de aprendizado
           <div className="mb-12">
             <h2 className="text-white">// Experiencias</h2>
             <div className="mt-6 mb-12 flex items-center gap-4 ">
-              <img className="w-24 rounded-full object-cover" src="https://media.licdn.com/dms/image/v2/C4E0BAQFuIkOtsvqC-A/company-logo_200_200/company-logo_200_200/0/1630643726740/grancoffeeoficial_logo?e=1735776000&v=beta&t=rixE8H8j3owNTLl763Ukn_hac4msfO6nHCu3cCU9YAM" alt="" />
+              <img className="w-24 rounded-full object-cover" src="https://yt3.googleusercontent.com/ytc/AIdro_kIBnf1KcYxgJA9vvOBC8dQHV3OpBsnbtFA1mJtvOFVhw=s160-c-k-c0x00ffffff-no-rj" alt="" />
               <div className="flex flex-col">
                 <h3 className="text-white">Gran Coffee</h3>
                 <span className="text-gray-400 italic">Developer Front-end</span>
@@ -32,7 +32,7 @@ Se você está procurando alguém com paixão, dedicação e sede de aprendizado
               </div>
             </div>
             <div className="mt-2 flex items-center gap-4">
-              <img className="w-24 rounded-full object-cover" src="https://media.licdn.com/dms/image/v2/D4D0BAQFWuY0nVSV7UA/company-logo_200_200/company-logo_200_200/0/1705518047331/jmocci_terraplanagem_logo?e=1735776000&v=beta&t=7RcAjWGJmuMN9TwyPvgeS_YPe5X9bGCfrkbsCqWQUGY" alt="" />
+              <img className="w-24 rounded-full object-cover" src="https://media.licdn.com/dms/image/v2/D4D0BAQFWuY0nVSV7UA/company-logo_200_200/company-logo_200_200/0/1705518047331/jmocci_terraplanagem_logo?e=1749081600&v=beta&t=4xqLHVyjummP0dVhqs0wcAitLxKzFtrSvu-zWk3Do1E" alt="" />
               <div className="flex flex-col">
                 <h3 className="text-white">J.MOCCI TERRAPLANAGEM</h3>
                 <span className="text-gray-400 italic">Developer Front-end</span>
@@ -44,7 +44,7 @@ Se você está procurando alguém com paixão, dedicação e sede de aprendizado
           <div>
             <h2 className="text-white">// Academy</h2>
             <div className="mt-6 mb-12 flex items-center gap-4">
-              <img className="w-24 rounded-full object-cover" src="https://media.licdn.com/dms/image/v2/D4D0BAQGWWA3gGWJ3Sg/company-logo_200_200/company-logo_200_200/0/1701903187680/rocketseat_logo?e=1736380800&v=beta&t=PZcww1b4tzgCROvGMTxuhTlkQXcPdJmz8FxsMy4K6wE" alt="" />
+              <img className="w-24 rounded-full object-cover" src="https://yt3.googleusercontent.com/SkyQrFYa4v7vVMpC4nqsdlOaIHWw1HvRUxf4940GcJfRWgGUxtMJ9WdTTJOCj0a1ddJYPKxe=s900-c-k-c0x00ffffff-no-rj" alt="" />
               <div className="flex flex-col">
                 <h3 className="text-white">Rocketseat</h3>
                 <span className="text-gray-400 italic">Developer Front-end & Mobile</span>
@@ -52,7 +52,7 @@ Se você está procurando alguém com paixão, dedicação e sede de aprendizado
               </div>
             </div>
             <div className="mt-2 flex items-center gap-4">
-              <img className="w-24 rounded-full object-cover" src="https://media.licdn.com/dms/image/v2/C560BAQEOFHwdORFPhA/company-logo_200_200/company-logo_200_200/0/1630639303809/unasp_br_logo?e=1736380800&v=beta&t=hkKEEx1pE7YyL_7MmYhWwZmn_cndm-6GWmdu9WQyXjc" alt="" />
+              <img className="w-24 rounded-full object-cover" src="https://yt3.googleusercontent.com/Xd8KszIxcsxeqsFYGVVfvzAn8d9n3ywevTT93-ZTUqh4qOR73kH8CGw8yRdzdKEAk_9O-v3P=s160-c-k-c0x00ffffff-no-rj" alt="" />
               <div className="flex flex-col">
                 <h3 className="text-white">Unasp HT</h3>
                 <span className="text-gray-400 italic">Sistema da Informação</span>
